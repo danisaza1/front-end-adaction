910fb4b2c5c3c7f558b3ef055e2a996dba952118
rchouhani <rchouhani@me.com>
2025-06-04 22:33:36 +0200

    ADD: handleSubmit function to fetch inputs values

--- app/client/profil/page.js
+++ app/client/profil/page.js
@@ -1,69 +1,87 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 
-export default function FormProfil () { 
-    const [ firstname, setFirstName ] = useState();
-    const [ lastname, setLastName ] = useState();
-    const [ location, setLocation ] = useState()
-    
-        async function fetchData(){
-          try {
-            const res = await fetch('http://localhost:3001/profil')
-            const volunteers = await res.json()
-            console.log('😵‍💫😵‍💫',volunteers)
-            
-            setFirstName(volunteers.firstname)
-            setLastName(volunteers.lastname)
-            setLocation(volunteers.location) 
-          }
-          catch (err) {
-          console.error("Erreur lors du chargement des volontaires :", err);
-          }
-        }           
-        useEffect(() =>{
-            fetchData()
-    }, [])
+export default function FormProfil() {
+  const [firstname, setFirstName] = useState();
+  const [lastname, setLastName] = useState();
+  const [location, setLocation] = useState();
 
-    const handleChangeFirstname = (e) => {
-        setFirstName(e.target.value)
+  async function fetchData() {
+    try {
+      const res = await fetch("http://localhost:3001/profil");
+      const volunteers = await res.json();
+      console.log("😵‍💫", volunteers);
+
+      setFirstName(volunteers.firstname);
+      setLastName(volunteers.lastname);
+      setLocation(volunteers.location);
+    } catch (err) {
+      console.error("Erreur lors du chargement des volontaires :", err);
     }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const handleChangeLastName = (e) => {
-        setLastName(e.target.value)
-    }
+  const handleChangeFirstname = (e) => {
+    setFirstName(e.target.value);
+  };
 
-    const handleChangeLocation = (e) => {
-        setLocation(e.target.value)
-    }
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
 
-    return (
-        <div className=" flex flex-col justify-evenly w-110 h-110 m-10">
-        <h2 className="text-4xl text-center font-bold">Votre Profil</h2>
-        <form   
-        className="flex flex-col">
-                <label className="text-xl">Name</label>
-            <input 
-                type="text"
-                name="name"
-                value={firstname}
-                onChange={handleChangeFirstname}
-            />
-            <label className="text-xl">Username</label>
-            <input
-                type="text"
-                name="username"
-                value={lastname}
-                onChange={handleChangeLastName}
-            />
-            <label className="text-xl">Ville</label>
-            <input
-            type="text"
-            name="location"
-            value={location}
-            onChange={handleChangeLocation}
-            />
-            <button className="bg-green-600 py-3 px-2 mt-7 rounded-lg shadow-lg">Update user</button>
-        </form>
+  const handleChangeLocation = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleSubmitUpdate = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/updateProfil', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+        firstname: firstname,
+        lastname: lastname,
+        location: location
+    })
+    })
+  }
+
+  return (
+    <div className=" flex flex-col justify-evenly w-110 h-110 m-10">
+      <h2 className="text-4xl text-center font-bold">Votre Profil</h2>
+      <form className="flex flex-col">
+        <label className="text-xl">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={firstname}
+          onChange={handleChangeFirstname}
+        />
+        <label className="text-xl">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={lastname}
+          onChange={handleChangeLastName}
+        />
+        <label className="text-xl">Ville</label>
+        <input
+          type="text"
+          name="location"
+          value={location}
+          onChange={handleChangeLocation}
+        />
+        <button
+          onClick={handleSubmitUpdate}
+          className="bg-green-600 py-3 px-2 mt-7 rounded-lg shadow-lg"
+        >
+          Update user
+        </button>
+      </form>
     </div>
-    )
+  );
 }
